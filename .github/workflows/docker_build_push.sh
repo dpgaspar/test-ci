1e32d7329f674cd5f2196ccd9be0bb9cc119ff3f
Daniel Gaspar <danielvazgaspar@gmail.com>
2021-01-14 10:05:46 +0000

    Add github actions workflows

---
+++ .github/workflows/docker_build_push.sh
@@ -0,0 +1,28 @@
+set -eo pipefail
+
+SHA=$(git rev-parse HEAD)
+REPO_NAME="test"
+
+if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
+  REFSPEC=$(echo "${GITHUB_HEAD_REF}" | sed 's/[^a-zA-Z0-9]/-/' | head -c 20)
+  PR_NUM=$(echo "${GITHUB_REF}" | sed 's:refs/pull/::' | sed 's:/merge::')
+  LATEST_TAG="pr-${PR_NUM}"
+elif [[ "${GITHUB_EVENT_NAME}" == "release" ]]; then
+  REFSPEC="${GITHUB_REF}"
+  LATEST_TAG="${REFSPEC}"
+else
+  REFSPEC=$(echo "${GITHUB_REF}" | sed 's:refs/heads/::' | sed 's/[^a-zA-Z0-9]/-/' | head -c 20)
+  LATEST_TAG="${REFSPEC}"
+fi
+
+if [[ "${REFSPEC}" == "master" ]]; then
+  LATEST_TAG="latest"
+fi
+
+cat<<EOF
+  Rolling with tags:
+  - ${REPO_NAME}:${SHA}
+  - ${REPO_NAME}:${REFSPEC}
+  - ${REPO_NAME}:${LATEST_TAG}
+EOF
+
